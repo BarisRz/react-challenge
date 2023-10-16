a04dee545ebcc5b463afc7b021a63741cea1b827
Baris G <barisg@pm.me>
2023-10-16 11:23:16 +0200

    fini sans CSS ni Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.scss";
-import CardList from "./components/CardList";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import CardList from "./components/CardList";
 import tableau from "./data/data.json";
+import Footer from "./components/Footer";
+const category = ["Tous", "Jeuxvideo", "Manga", "Anime"];
 function App() {
+  const [filter, setFilter] = useState("");
   return (
     <>
       <Navbar />
+      <div className="filtre">
+        {category.map((element) => (
+          <button>{element}</button>
+        ))}
+      </div>
       <CardList tableau={tableau} />
+      <Footer />
     </>
   );
 }
