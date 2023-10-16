9b677865aa361af090b640008ecd5587a7ef89fb
Baris G <barisg@pm.me>
2023-10-16 21:35:53 +0200

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import logo from "../assets/goatLogo.svg";
+import cart from "../assets/cart-shopping.svg";
 function Navbar({ prix, nombrePanier }) {
   return (
     <>
       <nav>
         <img src={logo} alt="logo" className="logo" />
-        <ul>
-          <li>Panier: {nombrePanier} </li>
-          <li>
-            {nombrePanier !== 1 && nombrePanier !== 0 ? "Articles" : "Article"}{" "}
-            {prix}€
-          </li>
-        </ul>
+        <p>
+          <img src={cart} className="logo" />
+          <ul>
+            <li>Panier: {nombrePanier} </li>
+            <li>
+              {nombrePanier !== 1 && nombrePanier !== 0
+                ? "Articles"
+                : "Article"}{" "}
+              {prix}€
+            </li>
+          </ul>
+        </p>
       </nav>
     </>
   );
